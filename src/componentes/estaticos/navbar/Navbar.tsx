f9699e1bc6096f1889f64723c3080fafa9dacc0b
Luferbero <luferberodrigues@gmail.com>
2023-06-15 16:35:53 -0300

    estilizando cores

--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -6,36 +6,36 @@ function Navbar() {
     return (
         <>
             <AppBar position="static">
-                <Toolbar variant="dense">
+                <Toolbar variant="dense" style={{ backgroundColor: "#2D2D2D" }}>
                     <Box style={{ cursor: "pointer" }} >
-                        <Typography variant="h5" color="inherit">
+                        <Typography variant="h5" style={{ color: "lightgrey" }}>
                             BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
                         <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
+                            <Typography variant="h6" style={{ color: "lightgrey" }}>
                                 home
                             </Typography>
                         </Box>
                         <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
+                            <Typography variant="h6" style={{ color: "lightgrey" }}>
                                 postagens
                             </Typography>
                         </Box>
                         <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
+                            <Typography variant="h6" style={{ color: "lightgrey" }}>
                                 temas
                             </Typography>
                         </Box>
                         <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
+                            <Typography variant="h6" style={{ color: "lightgrey" }}>
                                 cadastrar tema
                             </Typography>
                         </Box>
                         <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
+                            <Typography variant="h6" style={{ color: "lightgrey" }}>
                                 logout
                             </Typography>
                         </Box>
